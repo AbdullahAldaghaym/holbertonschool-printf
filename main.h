9345faca7f6dbeffd8c996b067ef7ee8010d9318
lilsouy <solaff.alessa@gmail.com>
2025-11-19 22:14:27 +0000

    Complete Task 4: Add u, o, x, X conversions
    - Implement print_unsigned for %u
    - Implement print_octal for %o
    - Implement print_hex for %x and %X
    - Handle all

--- main.h
+++ main.h
@@ -7,6 +7,8 @@ int _printf(const char *format, ...);
 int _putchar(char c);
 void print_number(int n, int *count);
 int print_binary(unsigned int n);
-
+int print_unsigned(unsigned int n);
+int print_octal(unsigned int n);
+int print_hex(unsigned int n, int uppercase);
 
 #endif
