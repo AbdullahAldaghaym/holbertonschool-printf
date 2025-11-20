f6a6b9e496332bbb582667041b52f11d0001a186
AbdullahAldaghaym <Abdullah97sd@gmail.com>
2025-11-20 21:58:59 +0000

    Complete Task 8: Implement flag characters +, space, #
    - Add comprehensive flag handling system with bitmask
    - + flag: Force sign for positive numbers (e.g., +42)
    - space flag: Print space for positive numbers (e.g., ' 42')
    - # flag: Alternative form for o, x, X conversions
    - Octal with #: 0 prefix (052 instead of 52)
    - Hexadecimal with #: 0x or 0X prefix (0x2a instead of 2a)
    - Maintain full backward compatibility
    - All previous specifiers (c, s, %, d, i, b, u, o, x, X, S, p) still work

--- _printf.c
+++ _printf.c
@@ -9,13 +9,11 @@ int buf_index = 0;
 int putchar_buffer(char c)
 {
 	output_buf[buf_index++] = c;
-
 	if (buf_index == 1024)
 	{
 		write(1, output_buf, buf_index);
 		buf_index = 0;
 	}
-
 	return (1);
 }
 
@@ -50,18 +48,9 @@ int get_flags(const char *format, int *i)
         }
         else if (format[*i] == ' ')
         {
-            if (format[*i + 1] == 'd' || format[*i + 1] == 'i' || 
-                format[*i + 1] == 'o' || format[*i + 1] == 'x' || 
-                format[*i + 1] == 'X' || format[*i + 1] == 'u')
-            {
-                flags |= FLAG_SPACE;
-                (*i)++;
-                found_flag = 1;
-            }
-            else
-            {
-                break;
-            }
+            flags |= FLAG_SPACE;
+            (*i)++;
+            found_flag = 1;
         }
         else if (format[*i] == '#')
         {
@@ -114,7 +103,6 @@ void print_number_int(unsigned int num, int *count)
 {
     if (num / 10)
         print_number_int(num / 10, count);
-
     *count += _putchar((num % 10) + '0');
 }
 
@@ -126,13 +114,9 @@ void print_number(int n, int *count, int flags)
     if (n == 0)
     {
         if (flags & FLAG_PLUS)
-        {
             *count += _putchar('+');
-        }
         else if (flags & FLAG_SPACE)
-        {
             *count += _putchar(' ');
-        }
         *count += _putchar('0');
         return;
     }
@@ -141,13 +125,9 @@ void print_number(int n, int *count, int flags)
     {
         is_negative = 1;
         if (n == INT_MIN)
-        {
             num = 2147483648U;
-        }
         else
-        {
             num = -n;
-        }
     }
     else
     {
@@ -157,19 +137,13 @@ void print_number(int n, int *count, int flags)
     if (!is_negative)
     {
         if (flags & FLAG_PLUS)
-        {
             *count += _putchar('+');
-        }
         else if (flags & FLAG_SPACE)
-        {
             *count += _putchar(' ');
-        }
     }
 
     if (is_negative)
-    {
         *count += _putchar('-');
-    }
 
     if (num / 10)
         print_number_int(num / 10, count);
@@ -180,42 +154,31 @@ void print_number(int n, int *count, int flags)
 int print_binary(unsigned int n)
 {
     int count = 0;
-
     if (n == 0)
         return _putchar('0');
-
     if (n / 2)
         count += print_binary(n / 2);
-
     count += _putchar((n % 2) + '0');
-
     return count;
 }
 
 int print_unsigned(unsigned int n)
 {
     int count = 0;
-
     if (n == 0)
         return _putchar('0');
-
     if (n / 10)
         count += print_unsigned(n / 10);
-
     count += _putchar((n % 10) + '0');
-
     return count;
 }
 
 int print_octal_recursive(unsigned int n)
 {
     int count = 0;
-
     if (n / 8)
         count += print_octal_recursive(n / 8);
-
     count += _putchar((n % 8) + '0');
-
     return count;
 }
 
@@ -223,88 +186,57 @@ int print_octal(unsigned int n, int flags)
 {
     int count = 0;
     int has_hash = (flags & FLAG_HASH) && (n != 0);
-
     if (n == 0)
     {
         count += _putchar('0');
         return count;
     }
-
     if (has_hash)
         count += _putchar('0');
-
     if (n / 8)
         count += print_octal_recursive(n / 8);
-
     count += _putchar((n % 8) + '0');
-
     return count;
 }
 
 int print_hex_recursive(unsigned int n, int uppercase)
 {
     int count = 0;
-    char *digits;
-
-    if (uppercase)
-        digits = "0123456789ABCDEF";
-    else
-        digits = "0123456789abcdef";
-
+    char *digits = uppercase ? "0123456789ABCDEF" : "0123456789abcdef";
     if (n / 16)
         count += print_hex_recursive(n / 16, uppercase);
-
     count += _putchar(digits[n % 16]);
-
     return count;
 }
 
 int print_hex(unsigned int n, int uppercase, int flags)
 {
     int count = 0;
-    char *digits;
+    char *digits = uppercase ? "0123456789ABCDEF" : "0123456789abcdef";
     int has_hash = (flags & FLAG_HASH) && (n != 0);
-
-    if (uppercase)
-        digits = "0123456789ABCDEF";
-    else
-        digits = "0123456789abcdef";
-
     if (n == 0)
     {
         count += _putchar('0');
         return count;
     }
-
     if (has_hash)
     {
         count += _putchar('0');
         count += _putchar(uppercase ? 'X' : 'x');
     }
-
     if (n / 16)
         count += print_hex_recursive(n / 16, uppercase);
-
     count += _putchar(digits[n % 16]);
-
     return count;
 }
 
 int print_hex_long(unsigned long n, int uppercase)
 {
     int count = 0;
-    char *digits;
-
-    if (uppercase)
-        digits = "0123456789ABCDEF";
-    else
-        digits = "0123456789abcdef";
-
+    char *digits = uppercase ? "0123456789ABCDEF" : "0123456789abcdef";
     if (n / 16)
         count += print_hex_long(n / 16, uppercase);
-
     count += _putchar(digits[n % 16]);
-
     return count;
 }
 
@@ -313,24 +245,19 @@ int print_custom_string(va_list args)
     char *str = va_arg(args, char *);
     int count = 0;
     unsigned char c;
-
     if (str == NULL)
         str = "(null)";
-
     while (*str)
     {
         c = (unsigned char)*str;
-        
         if (c < 32 || c >= 127)
         {
             count += _putchar('\\');
             count += _putchar('x');
-            
             if (c / 16 < 10)
                 count += _putchar((c / 16) + '0');
             else
                 count += _putchar((c / 16) - 10 + 'A');
-            
             if (c % 16 < 10)
                 count += _putchar((c % 16) + '0');
             else
@@ -342,7 +269,6 @@ int print_custom_string(va_list args)
         }
         str++;
     }
-
     return count;
 }
 
@@ -351,16 +277,11 @@ int print_pointer(va_list args)
     void *ptr = va_arg(args, void *);
     unsigned long address = (unsigned long)ptr;
     int count = 0;
-    
     if (ptr == NULL)
-    {
         return (_printf("(nil)"));
-    }
-    
     count += _putchar('0');
     count += _putchar('x');
     count += print_hex_long(address, 0);
-    
     return count;
 }
 
