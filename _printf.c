5fe30951fc510bbc26437fe990512b5326e4fb0b
lilsouy <solaff.alessa@gmail.com>
2025-11-20 20:17:30 +0000

    Complete Task 6: Implement %S custom conversion specifier
    - Add print_custom_string function for %S handling
    - Print non-printable characters (ASCII < 32 or >= 127) as \x followed by hex code
    - Always use 2-character uppercase hexadecimal representation
    - Maintain buffer system from previous tasks
    - Handle NULL strings gracefully with (null) output
    - Example: newline character prints as \x0A

--- _printf.c
+++ _printf.c
@@ -2,261 +2,303 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+/* Buffer Variables */
+char output_buf[1024];
+int buf_index = 0;
+
 /**
-	* flush_buffer - writes buffer to stdout
-	* @buffer: the buffer to flush
-	* @buf_ind: pointer to buffer index
-	*/
-void flush_buffer(char buffer[], int *buf_ind)
+ * putchar_buffer - adds character to buffer and flushes when full
+ * @c: character to add
+ * Return: 1 on success
+ */
+int putchar_buffer(char c)
 {
-	if (*buf_ind > 0)
+	output_buf[buf_index++] = c;
+
+	if (buf_index == 1024)
 	{
-	write(1, buffer, *buf_ind);
-	*buf_ind = 0;
+		write(1, output_buf, buf_index);
+		buf_index = 0;
 	}
-}
-
-/**
-	* add_to_buffer - adds character to buffer
-	* @c: character to add
-	* @buffer: the buffer
-	* @buf_ind: pointer to buffer index
-	* @count: pointer to character counter
-	*/
-void add_to_buffer(char c, char buffer[], int *buf_ind, int *count)
-{
-	if (*buf_ind >= BUFFER_SIZE)
-	flush_buffer(buffer, buf_ind);
-
-	buffer[(*buf_ind)++] = c;
-	(*count)++;
-}
-
-/**
-	* _putchar - writes the character c to stdout using buffer
-	* @c: The character to print
-	* Return: On success 1.
-	*/
-int _putchar(char c)
-{
-	/* للحفاظ على التوافق مع الدوال الأخرى */
-	static char local_buffer[BUFFER_SIZE];
-	static int local_buf_ind = 0;
-
-	if (local_buf_ind >= BUFFER_SIZE)
-	flush_buffer(local_buffer, &local_buf_ind);
-
-	local_buffer[local_buf_ind++] = c;
-
-	if (c == '\n' || local_buf_ind >= BUFFER_SIZE)
-	flush_buffer(local_buffer, &local_buf_ind);
 
 	return (1);
 }
 
 /**
-	* print_number - prints an integer number using buffer
-	* @n: number to print
-	* @count: pointer to character counter
-	* @buffer: the buffer
-	* @buf_ind: pointer to buffer index
-	*/
-void print_number(int n, int *count, char buffer[], int *buf_ind)
+ * flush_buffer - writes remaining buffer to stdout
+ */
+void flush_buffer(void)
+{
+	if (buf_index > 0)
+	{
+		write(1, output_buf, buf_index);
+		buf_index = 0;
+	}
+}
+
+/**
+ * _putchar - writes the character c to stdout using buffer
+ * @c: The character to print
+ * Return: On success 1.
+ */
+int _putchar(char c)
+{
+	return (putchar_buffer(c));
+}
+
+/**
+ * print_number - prints an integer number
+ * @n: number to print
+ * @count: pointer to character counter
+ */
+void print_number(int n, int *count)
 {
 	unsigned int num;
 
 	if (n == 0)
 	{
-	add_to_buffer('0', buffer, buf_ind, count);
-	return;
+		*count += _putchar('0');
+		return;
 	}
 
 	if (n < 0)
 	{
-	add_to_buffer('-', buffer, buf_ind, count);
-	num = -n;
+		*count += _putchar('-');
+		num = -n;
 	}
 	else
 	{
-	num = n;
+		num = n;
 	}
 
 	if (num / 10)
-	print_number(num / 10, count, buffer, buf_ind);
+		print_number(num / 10, count);
 
-	add_to_buffer((num % 10) + '0', buffer, buf_ind, count);
+	*count += _putchar((num % 10) + '0');
 }
 
 /**
-	* print_binary - prints a number in binary using buffer
-	* @n: number to print
-	* @buffer: the buffer
-	* @buf_ind: pointer to buffer index
-	* Return: number of characters printed
-	*/
-int print_binary(unsigned int n, char buffer[], int *buf_ind)
+ * print_binary - prints a number in binary
+ * @n: number to print
+ * Return: number of characters printed
+ */
+int print_binary(unsigned int n)
 {
 	int count = 0;
 
 	if (n == 0)
-	{
-	add_to_buffer('0', buffer, buf_ind, &count);
-	return count;
-	}
+		return _putchar('0');
 
 	if (n / 2)
-	count += print_binary(n / 2, buffer, buf_ind);
+		count += print_binary(n / 2);
 
-	add_to_buffer((n % 2) + '0', buffer, buf_ind, &count);
+	count += _putchar((n % 2) + '0');
+
 	return count;
 }
 
 /**
-	* print_unsigned - prints an unsigned integer using buffer
-	* @n: number to print
-	* @buffer: the buffer
-	* @buf_ind: pointer to buffer index
-	* Return: number of characters printed
-	*/
-int print_unsigned(unsigned int n, char buffer[], int *buf_ind)
+ * print_unsigned - prints an unsigned integer
+ * @n: number to print
+ * Return: number of characters printed
+ */
+int print_unsigned(unsigned int n)
 {
 	int count = 0;
+
+	if (n == 0)
+		return _putchar('0');
 
 	if (n / 10)
-	count += print_unsigned(n / 10, buffer, buf_ind);
+		count += print_unsigned(n / 10);
 
-	add_to_buffer((n % 10) + '0', buffer, buf_ind, &count);
+	count += _putchar((n % 10) + '0');
+
 	return count;
 }
 
 /**
-	* print_octal - prints a number in octal using buffer
-	* @n: number to print
-	* @buffer: the buffer
-	* @buf_ind: pointer to buffer index
-	* Return: number of characters printed
-	*/
-int print_octal(unsigned int n, char buffer[], int *buf_ind)
+ * print_octal - prints a number in octal
+ * @n: number to print
+ * Return: number of characters printed
+ */
+int print_octal(unsigned int n)
 {
 	int count = 0;
 
-	if (n / 8)
-	count += print_octal(n / 8, buffer, buf_ind);
+	if (n == 0)
+		return _putchar('0');
 
-	add_to_buffer((n % 8) + '0', buffer, buf_ind, &count);
+	if (n / 8)
+		count += print_octal(n / 8);
+
+	count += _putchar((n % 8) + '0');
+
 	return count;
 }
 
 /**
-	* print_hex - prints a number in hexadecimal using buffer
-	* @n: number to print
-	* @buffer: the buffer
-	* @buf_ind: pointer to buffer index
-	* @uppercase: 1 for uppercase, 0 for lowercase
-	* Return: number of characters printed
-	*/
-int print_hex(unsigned int n, char buffer[], int *buf_ind, int uppercase)
+ * print_hex - prints a number in hexadecimal
+ * @n: number to print
+ * @uppercase: 1 for uppercase, 0 for lowercase
+ * Return: number of characters printed
+ */
+int print_hex(unsigned int n, int uppercase)
 {
 	int count = 0;
 	char *digits;
 
 	if (uppercase)
-	digits = "0123456789ABCDEF";
+		digits = "0123456789ABCDEF";
 	else
-	digits = "0123456789abcdef";
+		digits = "0123456789abcdef";
+
+	if (n == 0)
+		return _putchar('0');
 
 	if (n / 16)
-	count += print_hex(n / 16, buffer, buf_ind, uppercase);
+		count += print_hex(n / 16, uppercase);
 
-	add_to_buffer(digits[n % 16], buffer, buf_ind, &count);
+	count += _putchar(digits[n % 16]);
+
 	return count;
 }
 
 /**
-	* _printf - produces output according to a format using buffer
-	* @format: character string containing directives
-	* Return: number of characters printed
-	*/
+ * print_custom_string - prints string with special handling for non-printable chars
+ * @args: va_list arguments
+ * Return: number of characters printed
+ */
+int print_custom_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+	int count = 0;
+	unsigned char c;
+
+	if (str == NULL)
+		str = "(null)";
+
+	while (*str)
+	{
+		c = (unsigned char)*str;
+		
+		if (c < 32 || c >= 127)
+		{
+			count += _putchar('\\');
+			count += _putchar('x');
+			
+			if (c / 16 < 10)
+				count += _putchar((c / 16) + '0');
+			else
+				count += _putchar((c / 16) - 10 + 'A');
+			
+			if (c % 16 < 10)
+				count += _putchar((c % 16) + '0');
+			else
+				count += _putchar((c % 16) - 10 + 'A');
+		}
+		else
+		{
+			count += _putchar(c);
+		}
+		str++;
+	}
+
+	return count;
+}
+
+/**
+ * _printf - produces output according to a format
+ * @format: character string containing directives
+ * Return: number of characters printed
+ */
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int i = 0, count = 0, buf_ind = 0;
-	char buffer[BUFFER_SIZE];
+	int i = 0, count = 0;
 	char *str;
 
 	if (format == NULL)
-	return (-1);
+		return (-1);
+
+	/* Reset buffer at start */
+	buf_index = 0;
 
 	va_start(args, format);
 
 	while (format[i] != '\0')
 	{
-	if (format[i] == '%')
-	{
-	i++;
-	if (format[i] == '\0')
-	{
-	flush_buffer(buffer, &buf_ind);
-	return (-1);
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == '\0')
+			{
+				flush_buffer();
+				va_end(args);
+				return (-1);
+			}
+				
+			if (format[i] == 'c')
+			{
+				count += _putchar(va_arg(args, int));
+			}
+			else if (format[i] == 's')
+			{
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(null)";
+				while (str[0] != '\0')
+				{
+					count += _putchar(str[0]);
+					str++;
+				}
+			}
+			else if (format[i] == 'S')
+			{
+				count += print_custom_string(args);
+			}
+			else if (format[i] == 'd' || format[i] == 'i')
+			{
+				print_number(va_arg(args, int), &count);
+			}
+			else if (format[i] == 'b')
+			{
+				count += print_binary(va_arg(args, unsigned int));
+			}
+			else if (format[i] == 'u')
+			{
+				count += print_unsigned(va_arg(args, unsigned int));
+			}
+			else if (format[i] == 'o')
+			{
+				count += print_octal(va_arg(args, unsigned int));
+			}
+			else if (format[i] == 'x')
+			{
+				count += print_hex(va_arg(args, unsigned int), 0);
+			}
+			else if (format[i] == 'X')
+			{
+				count += print_hex(va_arg(args, unsigned int), 1);
+			}
+			else if (format[i] == '%')
+			{
+				count += _putchar('%');
+			}
+			else
+			{
+				count += _putchar('%');
+				count += _putchar(format[i]);
+			}
+		}
+		else
+		{
+			count += _putchar(format[i]);
+		}
+		i++;
 	}
 
-	if (format[i] == 'c')
-	{
-	add_to_buffer(va_arg(args, int), buffer, &buf_ind, &count);
-	}
-	else if (format[i] == 's')
-	{
-	str = va_arg(args, char *);
-	if (str == NULL)
-	str = "(null)";
-	while (str[0] != '\0')
-	{
-	add_to_buffer(str[0], buffer, &buf_ind, &count);
-	str++;
-	}
-	}
-	else if (format[i] == 'd' || format[i] == 'i')
-	{
-	print_number(va_arg(args, int), &count, buffer, &buf_ind);
-	}
-	else if (format[i] == 'b')
-	{
-	count += print_binary(va_arg(args, unsigned int), buffer, &buf_ind);
-	}
-	else if (format[i] == 'u')
-	{
-	count += print_unsigned(va_arg(args, unsigned int), buffer, &buf_ind);
-	}
-	else if (format[i] == 'o')
-	{
-	count += print_octal(va_arg(args, unsigned int), buffer, &buf_ind);
-	}
-	else if (format[i] == 'x')
-	{
-	count += print_hex(va_arg(args, unsigned int), buffer, &buf_ind, 0);
-	}
-	else if (format[i] == 'X')
-	{
-	count += print_hex(va_arg(args, unsigned int), buffer, &buf_ind, 1);
-	}
-	else if (format[i] == '%')
-	{
-	add_to_buffer('%', buffer, &buf_ind, &count);
-	}
-	else
-	{
-	add_to_buffer('%', buffer, &buf_ind, &count);
-	add_to_buffer(format[i], buffer, &buf_ind, &count);
-	}
-	}
-	else
-	{
-	add_to_buffer(format[i], buffer, &buf_ind, &count);
-	}
-	i++;
-	}
-
-	flush_buffer(buffer, &buf_ind);
+	flush_buffer();
 	va_end(args);
 	return (count);
 }
--- main.h
+++ main.h
@@ -13,4 +13,5 @@ int print_hex(unsigned int n, int uppercase);
 int putchar_buffer(char c);
 void flush_buffer(void);
 int print_custom_string(va_list args); 
+
 #endif
